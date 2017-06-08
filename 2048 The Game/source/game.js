879538d768d5646d5c090956fbd8bf40698c6d1d
Ilya Protasov <pfltriot@gmail.com>
2017-06-08 22:36:33 +0300

    Add files via upload

--- 2048 The Game/source/game.js
+++ 2048 The Game/source/game.js
@@ -1,16 +1,16 @@
-var scoreForm = document.getElementById('score_number');
-var bestScoreForm = document.getElementById('best_score_number');
-var newGameButton = document.getElementById('new_game_button');
+var COUNT_OF_CELLS = 4;
+var DEFAULT_CELL = 2;
+
+var scoreForm = document.getElementById('scoreNumber');
+var bestScoreForm = document.getElementById('bestScoreNumber');
+var newGameButton = document.getElementById('newGameButton');
 var score = 0;
 var bestScore = 0;
 var endOfGame = false;
 
-function restartGame() {
-    newGameButton.onclick = function () {
-        cleanCanvas();
-        mainGame();
-    };
-}
+newGameButton.onklick = function () {
+    initGame();
+};
 
 function mainGame() {
     initGame();
@@ -24,15 +24,20 @@ function mainGame() {
 
 function endGame() {
     canvas.style.opacity = '0.3';
+    canvas.style.transitionDuration = '0.5s';
 }
 
 function initGame() {
-    var COUNT_OF_CELLS = 4;
     var theField = [];
 
-    fieldInit();
-    
-    function fieldInit() {
+    fieldInit(theField);
+    drawField(theField);
+
+    for (var k = 1; k <= DEFAULT_CELL; ++k) {
+        addNewCell(theField);
+    }
+
+    function fieldInit(theField) {
         for (var i = 0; i < COUNT_OF_CELLS; ++i) {
             theField[i] = [];
             for (var j = 0; j < COUNT_OF_CELLS; ++j) {
@@ -40,18 +45,18 @@ function initGame() {
             }
         }
 
-        function Cell(xPos, yPos) {
-            this.number = 0;
-            this.x = xPos;
-            this.y = yPos;
+        function Cell(xCord, yCord) {
+            this.value = 0;
+            this.x = xCord * 100 + 15 * xCord;
+            this.y = yCord * 100 + 15 * yCord;
         }
     } // создание и заполнение поля ячейками если number = 0 то их посути нет
 }
-
+//чистим канваз для начала новой игры
 function cleanCanvas() {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
 }
-
+//keyInputManager
 document.onkeydown = function(event) {
     if (!endOfGame) {
         switch (event.keyCode) {
@@ -80,24 +85,29 @@ document.onkeydown = function(event) {
         }
     }
 };
-
-function drawField() {
+//отрисовка текущего состояния поля
+function drawField(theField) {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext('2d');
     var WIDTH = 445;
     var HEIGHT = 445;
+    var CELL_SIZE = 100;
 
     canvas.width = WIDTH;
     canvas.height = HEIGHT;
 
-    drawCell(0, 0, 100, 2);
-    drawCell(115, 0, 100, 64);
     drawBackground();
 
-    function drawCell(cellX, cellY, cellSize, cellNum) {
+    for (var i = 0; i < COUNT_OF_CELLS; ++i) {
+        for (var j = 0; j < COUNT_OF_CELLS; ++j) {
+            drawCell(theField[i][j]);
+        }
+    }
+//рисуем ячейки
+    function drawCell(Cell) {
         var colorF;
-        if (cellNum != 0) {
-            switch (cellNum){
+        if (Cell.value != 0) {
+            switch (Cell.value) {
                 case 0 : colorF = "#CDC1B3"; break;
                 case 2 : colorF = "#EEE4DA"; break;
                 case 4 : colorF = "#EEF4DE"; break;
@@ -115,18 +125,18 @@ function drawField() {
 
             ctx.fillStyle = colorF;
             ctx.strokeStyle = '#BAAEA0';
-            ctx.fillRect(cellX, cellY, cellSize, cellSize);
-            ctx.strokeRect(cellX, cellY, cellSize, cellSize);
+            ctx.fillRect(Cell.x, Cell.y, CELL_SIZE, CELL_SIZE);
+            ctx.strokeRect(Cell.x, Cell.y, CELL_SIZE, CELL_SIZE);
             ctx.beginPath();
             ctx.fillStyle = '#776e65';
             ctx.textAlign = 'center';
             ctx.font = "60px Mogra";
-            ctx.fillText(cellNum, cellX + 50, cellY + 70);
+            ctx.fillText(Cell.value, Cell.x + 50, Cell.y + 70);
             ctx.fill();
             ctx.closePath();
         }
     }
-
+//отрисовываем задний план(линии)
     function drawBackground() {
         var xBackH = 100;
         var yBackH = 0;
@@ -151,4 +161,32 @@ function drawField() {
         }
     }
 }
-drawField();
+
+function addNewCell(theField) {
+    var freeCell = false;
+    var emptyCell = false;
+
+    for (var i = 0; i < COUNT_OF_CELLS; ++i) {
+        for (var j = 0; j < COUNT_OF_CELLS; ++j) {
+            if(!theField[i][j].value) {
+                freeCell = true;
+            }
+        }
+    }
+
+    if (!freeCell) {
+        endOfGame = true;
+    } else {
+        while(!emptyCell) {
+            var xCord = Math.floor(Math.random() * COUNT_OF_CELLS);
+            var yCord = Math.floor(Math.random() * COUNT_OF_CELLS);
+            if (!theField[xCord][yCord].value) {
+                theField[xCord][yCord].value = 2 * Math.ceil(Math.random() * 2);
+                drawField(theField);
+                emptyCell = true;
+            }
+        }
+    }
+}
+
+initGame();
