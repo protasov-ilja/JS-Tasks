3998656b519bb37683049f21cc5cba1441f27395
Ilya Protasov <pfltriot@gmail.com>
2017-06-18 21:33:24 +0300

    Add files via upload

--- 2048 The Game/source/game.js
+++ 2048 The Game/source/game.js
@@ -4,24 +4,23 @@ var DEFAULT_CELL = 2;
 var scoreForm = document.getElementById('scoreNumber');
 var bestScoreForm = document.getElementById('bestScoreNumber');
 var newGameButton = document.getElementById('newGameButton');
-var score = 0;
+var score;
 var bestScore = 0;
 var endOfGame = false;
+var theField = [];
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
+var WIDTH = 445;
+var HEIGHT = 445;
+var CELL_SIZE = 100;
 
 newGameButton.onclick = function (event) {
     event.preventDefault();
+    score = 0;
+    canvas.style.opacity = '1';
+    canvas.style.transitionDuration = '0.5s';
     initGame();
 };
-
-function mainGame() {
-    initGame();
-
-    while(!endOfGame) {
-       startGame(theField);
-    }
-
-    endGame();
-}
 
 function endGame() {
     canvas.style.opacity = '0.3';
@@ -29,49 +28,51 @@ function endGame() {
 }
 
 function initGame() {
-    var theField = [];
-
     fieldInit(theField);
-    drawField(theField);
+    drawField();
 
-    for (var k = 1; k <= DEFAULT_CELL; ++k) {
-        addNewCell(theField);
+    for (var AmountCell = 1; AmountCell <= DEFAULT_CELL; ++AmountCell) {
+        addNewCell();
     }
+}
 
-    function fieldInit(theField) {
-        for (var i = 0; i < COUNT_OF_CELLS; ++i) {
-            theField[i] = [];
-            for (var j = 0; j < COUNT_OF_CELLS; ++j) {
-                theField[i][j] = new Cell(i, j);
-            }
+function fieldInit(theField) {
+    for (var posY = 0; posY < COUNT_OF_CELLS; ++posY) {
+        theField[posY] = [];
+        for (var posX = 0; posX < COUNT_OF_CELLS; ++posX) {
+            theField[posY][posX] = new Cell(posY, posX);
         }
+    }
+} // создание и заполнение поля ячейками если number = 0 то их посути нет
 
-        function Cell(xCord, yCord) {
-            this.value = 0;
-            this.x = xCord * 100 + 15 * xCord;
-            this.y = yCord * 100 + 15 * yCord;
-        }
-    } // создание и заполнение поля ячейками если number = 0 то их посути нет
+function Cell(yCord, xCord) {
+    var cellSize = 100;
+    var lineSize = 15;
+
+    this.value = 0;
+    this.x = xCord * cellSize + lineSize * xCord;
+    this.y = yCord * cellSize + lineSize * yCord;
 }
 
 document.onkeydown = function (event) {
+    event.preventDefault();
     if (!endOfGame) {
         switch (event.keyCode) {
             case 38:
             case 87:
-                moveUp(theField);
+                moveUp();
                 break;
             case 39:
             case 68:
-                moveRight(theField);
+                moveRight();
                 break;
             case 40:
             case 83:
-                moveDown(theField);
+                moveDown();
                 break;
             case 37:
             case 65:
-                moveLeft(theField);
+                moveLeft();
         }
 
         scoreForm.innerHTML = score;
@@ -81,195 +82,206 @@ document.onkeydown = function (event) {
             bestScoreForm.innerHTML = bestScore;
         }
     }
-
-    function moveRight(theField) {
-        for (var j = 0; j < COUNT_OF_CELLS; ++j) {
-            for (var i = COUNT_OF_CELLS; i == 0; --i) {
-                if (theField[i][j].value) {
-                    var duplicate = false;
-                    var currX = i;
-                    while (currX < COUNT_OF_CELLS - i && !duplicate) {
-                        if (!theField[currX + 1][j].value) {
-                            theField[currX + 1][j].value = theField[currX][j].value;
-                            theField[currX][j].value = 0;
-                            currX++;
-                        } else if (theField[i][j].value == theField[currX + 1][j].value) {
-                            theField[currX + 1][j].value *= 2;
-                            score += theField[currX + 1][j].value;
-                            theField[currX][j].value = 0;
-                            duplicate = true;
-                        } else {
-                            duplicate = true;
-                        }
-                    }
-                }
-            }
-        }
-        addNewCell(theField);
-    }
-
-    function moveLeft(theField) {
-        for (var j = 0; j < COUNT_OF_CELLS; ++j) {
-            for (var i = 0; i < COUNT_OF_CELLS; ++i) {
-                if (theField[i][j].value) {
-                    var duplicate = false;
-                    var currX = i;
-                    while (currX < 0 && !duplicate) {
-                        if (!theField[currX - 1][j].value) {
-                            theField[currX - 1][j].value = theField[currX][j].value;
-                            theField[currX][j].value = 0;
-                            currX--;
-                        } else if (theField[i][j].value == theField[currX - 1][j].value) {
-                            theField[currX - 1][j].value *= 2;
-                            score += theField[currX - 1][j].value;
-                            theField[currX][j].value = 0;
-                            duplicate = true;
-                        } else {
-                            duplicate = true;
-                        }
-                    }
-                }
-            }
-        }
-        addNewCell(theField);
-    }
-
-    function moveUp(theField) {
-        for (var i = 0; i < COUNT_OF_CELLS; ++i) {
-            for (var j = 0; j < COUNT_OF_CELLS; ++j) {
-                if (theField[i][j].value) {
-                    var duplicate = false;
-                    var currY = j;
-                    while (currY < 0 && !duplicate) {
-                        if (!theField[currY - 1][j].value) {
-                            theField[currY - 1][j].value = theField[currY][j].value;
-                            theField[currY][j].value = 0;
-                            currY--;
-                        } else if (theField[i][j].value == theField[currY + 1][j].value) {
-                            theField[currY - 1][j].value *= 2;
-                            score += theField[currY - 1][j].value;
-                            theField[currY][j].value = 0;
-                            duplicate = true;
-                        } else {
-                            duplicate = true;
-                        }
-                    }
-                }
-            }
-        }
-        addNewCell(theField);
-    }
-
-    function moveDown(theField) {
-        for (var i = 0; i < COUNT_OF_CELLS; ++i) {
-            for (var j = COUNT_OF_CELLS; j == 0; --j) {
-                if (theField[i][j].value) {
-                    var duplicate = false;
-                    var currY = j;
-                    while (currY < COUNT_OF_CELLS - j && !duplicate) {
-                        if (!theField[currY + 1][j].value) {
-                            theField[currY + 1][j].value = theField[currY][j].value;
-                            theField[currY][j].value = 0;
-                            currY++;
-                        } else if (theField[i][j].value == theField[currY + 1][j].value) {
-                            theField[currY + 1][j].value *= 2;
-                            score += theField[currY + 1][j].value;
-                            theField[currY][j].value = 0;
-                            duplicate = true;
-                        } else {
-                            duplicate = true;
-                        }
-                    }
-                }
-            }
-        }
-        addNewCell(theField);
-    }
 };
 
+function moveRight() {//частично готово
+    var CELLS_AMOUNT = COUNT_OF_CELLS - 1;
+
+    for (var yPos = 0; yPos < COUNT_OF_CELLS; ++yPos) {
+        for (var xPos = CELLS_AMOUNT; xPos >= 0; --xPos) {
+            if (theField[yPos][xPos].value != 0) {
+                var duplicate = false;
+                var currX = xPos;
+                while ( (currX + 1 < COUNT_OF_CELLS) && !duplicate) {
+                    if (!theField[yPos][currX + 1].value) {
+                        theField[yPos][currX + 1].value = theField[yPos][currX].value;
+                        theField[yPos][currX].value = 0;
+                        //анимация движения
+                        currX++;
+                    } else if (theField[yPos][currX].value == theField[yPos][currX + 1].value) {
+                        theField[yPos][currX + 1].value *= 2;
+                        score += theField[yPos][currX + 1].value;
+                        theField[yPos][currX].value = 0;
+                        //анимация движения + анимация слияния
+                        duplicate = true;
+                    } else {
+                        duplicate = true;
+                    }
+                }
+            }
+        }
+    }
+    addNewCell();
+}
+
+function moveLeft() {//частично готово
+    for (var yPos = 0; yPos < COUNT_OF_CELLS; ++yPos) {
+        for (var xPos = 1; xPos < COUNT_OF_CELLS; ++xPos) {
+            if (theField[yPos][xPos].value != 0) {
+                var duplicate = false;
+                var currX = xPos;
+                while ( (currX > 0) && !duplicate) {
+                    if (!theField[yPos][currX - 1].value) {
+                        theField[yPos][currX - 1].value = theField[yPos][currX].value;
+                        theField[yPos][currX].value = 0;
+                        //анимация движения
+                        currX--;
+                    } else if (theField[yPos][currX].value == theField[yPos][currX - 1].value) {
+                        theField[yPos][currX - 1].value *= 2;
+                        score += theField[yPos][currX - 1].value;
+                        theField[yPos][currX].value = 0;
+                        //анимация движения + анимация слияния
+                        duplicate = true;
+                    } else {
+                        duplicate = true;
+                    }
+                }
+            }
+        }
+    }
+    addNewCell();
+}
+
+function moveUp() {//частично готово
+    for (var xPos = 0; xPos < COUNT_OF_CELLS; ++xPos) {
+        for (var yPos = 1; yPos < COUNT_OF_CELLS; ++yPos) {
+            if (theField[yPos][xPos].value != 0) {
+                var duplicate = false;
+                var currY = yPos;
+                while ( (currY >= 0) && !duplicate) {
+                    if (!theField[currY - 1][xPos].value) {
+                        theField[currY - 1][xPos].value = theField[currY][xPos].value;
+                        theField[currY][xPos].value = 0;
+                        //анимация движения
+                        currY--;
+                    } else if (theField[currY][xPos].value == theField[currY - 1][xPos].value) {
+                        theField[currY - 1][xPos].value *= 2;
+                        score += theField[currY - 1][xPos].value;
+                        theField[currY][xPos].value = 0;
+                        //анимация движения + анимация слияния
+                        duplicate = true;
+                    } else {
+                        duplicate = true;
+                    }
+                }
+            }
+        }
+    }
+    addNewCell();
+}
+
+function moveDown() {
+    var CELLS_AMOUNT = COUNT_OF_CELLS - 1;
+
+    for (var xPos = 0; xPos < COUNT_OF_CELLS; ++xPos) {
+        for (var yPos = CELLS_AMOUNT; yPos >= 0; --yPos) {
+            if (theField[yPos][xPos].value != 0) {
+                var duplicate = false;
+                var currY = yPos;
+                while ( (currY + 1 < COUNT_OF_CELLS) && !duplicate) {
+                    if (!theField[currY + 1][xPos].value) {
+                        theField[currY + 1][xPos].value = theField[currY][xPos].value;
+                        theField[currY][xPos].value = 0;
+                        //анимация движения
+                        currY++;
+                    } else if (theField[yPos][xPos].value == theField[currY + 1][xPos].value) {
+                        theField[currY + 1][xPos].value *= 2;
+                        score += theField[currY + 1][xPos].value;
+                        theField[currY][xPos].value = 0;
+                        //анимация движения + анимация слияния
+                        duplicate = true;
+                    } else {
+                        duplicate = true;
+                    }
+                }
+            }
+        }
+    }
+    addNewCell();
+}
 
 //отрисовка текущего состояния поля
-function drawField(theField) {
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext('2d');
-    var WIDTH = 445;
-    var HEIGHT = 445;
-    var CELL_SIZE = 100;
-
+function drawField() {
     canvas.width = WIDTH;
     canvas.height = HEIGHT;
 
     drawBackground();
 
-    for (var i = 0; i < COUNT_OF_CELLS; ++i) {
-        for (var j = 0; j < COUNT_OF_CELLS; ++j) {
-            drawCell(theField[i][j]);
-        }
-    }
-//рисуем ячейки
-    function drawCell(Cell) {
-        var colorF;
-        if (Cell.value != 0) {
-            switch (Cell.value) {
-                case 2 : colorF = "#EEE4DA"; break;
-                case 4 : colorF = "#ECE0C8"; break;
-                case 8 : colorF = "#F5AF7C"; break;
-                case 16 : colorF = "#F59563"; break;
-                case 32 : colorF = "#F27C3F"; break;
-                case 64 : colorF = "#F3651C"; break;
-                case 128 : colorF = "#E9A512"; break;
-                case 256 : colorF = "#F8BC3C"; break;
-                case 512 : colorF = "#EDC850"; break;
-                case 1024 : colorF = "#E91A1A"; break;
-                case 2048 : colorF = "#EDC63D"; break;
-            }
-
-            ctx.fillStyle = colorF;
-            ctx.strokeStyle = '#BAAEA0';
-            ctx.fillRect(Cell.x, Cell.y, CELL_SIZE, CELL_SIZE);
-            ctx.strokeRect(Cell.x, Cell.y, CELL_SIZE, CELL_SIZE);
-            ctx.beginPath();
-            ctx.fillStyle = '#776e65';
-            ctx.textAlign = 'center';
-            ctx.font = "60px Mogra";
-            ctx.fillText(Cell.value, Cell.x + 50, Cell.y + 70);
-            ctx.fill();
-            ctx.closePath();
-        }
-    }
-//отрисовываем задний план(линии)
-    function drawBackground() {
-        var xBackH = 100;
-        var yBackH = 0;
-        var k = 0;
-
-        for (var i = 1; i <= 3; ++i) {
-            ctx.fillStyle = '#BAAEA0';
-            ctx.fillRect(xBackH + k, yBackH, 15, HEIGHT);
-            k += 15;
-            xBackH += 100;
-        }
-
-        var xBackW = 0;
-        var yBackW = 100;
-        var g = 0;
-
-        for (var j = 1; j <= 3; ++j) {
-            ctx.fillStyle = '#BAAEA0';
-            ctx.fillRect(xBackW, yBackW + g, WIDTH, 15);
-            g += 15;
-            yBackW += 100;
+    for (var yPos = 0; yPos < COUNT_OF_CELLS; ++yPos) {
+        for (var xPos = 0; xPos < COUNT_OF_CELLS; ++xPos) {
+            drawCell(theField[yPos][xPos]);
         }
     }
 }
 
-function addNewCell(theField) {
+//рисуем ячейки
+function drawCell(Cell) {
+    var colorF;
+    var posValueX = 50;
+    var posValueY = 70;
+    var fontFamily = "60px Mogra";
+
+    if (Cell.value != 0) {
+        switch (Cell.value) {
+            case 2 : colorF = "#EEE4DA"; break;
+            case 4 : colorF = "#ECE0C8"; break;
+            case 8 : colorF = "#F5AF7C"; break;
+            case 16 : colorF = "#F59563"; break;
+            case 32 : colorF = "#F27C3F"; break;
+            case 64 : colorF = "#F3651C"; break;
+            case 128 : colorF = "#E9A512"; break;
+            case 256 : colorF = "#F8BC3C"; break;
+            case 512 : colorF = "#EDC850"; break;
+            case 1024 : colorF = "#E91A1A"; break;
+            case 2048 : colorF = "#EDC63D"; break;
+        }
+
+        ctx.fillStyle = colorF;
+        ctx.strokeStyle = '#BAAEA0';
+        ctx.fillRect(Cell.x, Cell.y, CELL_SIZE, CELL_SIZE);
+        ctx.strokeRect(Cell.x, Cell.y, CELL_SIZE, CELL_SIZE);
+        ctx.beginPath();
+        ctx.fillStyle = '#776e65';
+        ctx.textAlign = 'center';
+        ctx.font = fontFamily;
+        ctx.fillText(Cell.value, Cell.x + posValueX, Cell.y + posValueY);
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+//отрисовываем задний план(линии)
+function drawBackground() {
+    var xBackH = 100;
+    var yBackH = 0;
+    var lWidth = 0;
+    var lineWidth = 15;
+
+    for (var yPos = 1; yPos <= 3; ++yPos) {
+        ctx.fillStyle = '#BAAEA0';
+        ctx.fillRect(xBackH + lWidth, yBackH, lineWidth, HEIGHT);
+        lWidth += 15;
+        xBackH += 100;
+    }
+
+    var xBackW = 0;
+    var yBackW = 100;
+    var lHeight = 0;
+
+    for (var xPos = 1; xPos <= 3; ++xPos) {
+        ctx.fillStyle = '#BAAEA0';
+        ctx.fillRect(xBackW, yBackW + lHeight, WIDTH, lineWidth);
+        lHeight += 15;
+        yBackW += 100;
+    }
+}
+
+function addNewCell() {
     var freeCell = false;
     var emptyCell = false;
 
-    for (var i = 0; i < COUNT_OF_CELLS; ++i) {
-        for (var j = 0; j < COUNT_OF_CELLS; ++j) {
-            if(!theField[i][j].value) {
+    for (var yPos = 0; yPos < COUNT_OF_CELLS; ++yPos) {
+        for (var xPos = 0; xPos < COUNT_OF_CELLS; ++xPos) {
+            if(!theField[yPos][xPos].value) {
                 freeCell = true;
             }
         }
@@ -277,13 +289,15 @@ function addNewCell(theField) {
 
     if (!freeCell) {
         endOfGame = true;
+        endGame();
     } else {
         while(!emptyCell) {
             var xCord = Math.floor(Math.random() * COUNT_OF_CELLS);
             var yCord = Math.floor(Math.random() * COUNT_OF_CELLS);
-            if (!theField[xCord][yCord].value) {
-                theField[xCord][yCord].value = 2 * Math.ceil(Math.random() * 2);
-                drawField(theField);
+
+            if (!theField[yCord][xCord].value) {
+                theField[yCord][xCord].value = 2 * Math.ceil(Math.random() * 2);
+                drawField();
                 emptyCell = true;
             }
         }
