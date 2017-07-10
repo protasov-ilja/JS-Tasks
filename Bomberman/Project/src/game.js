ae34cfa808d527659f3bb6e90fd4079727447147
Ilya Protasov <pfltriot@gmail.com>
2017-07-10 19:38:26 +0300

    Add files via upload

--- Bomberman/Project/src/game.js
+++ Bomberman/Project/src/game.js
@@ -1,37 +1,167 @@
-var COUNT_OF_CELLS_WIDTH = 16;
-var COUNT_OF_CELLS_HEIGHT = 13;
-var WIDTH = 480;
-var HEIGHT = 390;
-var CELL_SIZE = 30;
-var score = 0;
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext('2d');
+const COUNT_OF_CELLS_WIDTH = 16;
+const COUNT_OF_CELLS_HEIGHT = 13;
+const WIDTH = 480;
+const HEIGHT = 390;
+const CELL_SIZE = 30;
+const PLAYER_SPEED = 10;
+let score = 0;
+let endOfGame = false;
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext('2d');
+let spriteBlock = new Image(); // "Создаём" изображение
+let spriteHero = new Image();
 
-drawField();
+class Player {
+    constructor() {
+        this.posX = 30;
+        this.posY = 30;
+    }
+}
+
+let user = new Player();
+
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+// Источник изображения
+spriteHero.src = 'img/sprites/sprite_hero.png';
+spriteBlock.src = 'img/sprites/sprite_block.png';
+
+initGame();
+
+
+document.onkeydown = function (event) {
+    if (!endOfGame) {
+        switch (event.keyCode) {
+            case 38:
+            case 87:
+                event.preventDefault();
+                moveUp();
+                break;
+            case 39:
+            case 68:
+                event.preventDefault();
+                moveRight();
+                break;
+            case 40:
+            case 83:
+                event.preventDefault();
+                moveDown();
+                break;
+            case 37:
+            case 65:
+                event.preventDefault();
+                moveLeft();
+        }
+    }
+};
+
+function moveUp() {
+    let nextPos = user.posY - PLAYER_SPEED;
+
+    if (nextPos >= CELL_SIZE) {
+        user.posY = nextPos;
+
+        drawGame(user.posX, user.posY);
+    }
+}
+
+function moveDown() {
+    let nextPos = user.posY + PLAYER_SPEED;
+    const MAX_HEIGHT = HEIGHT - (CELL_SIZE * 2);
+
+    if (nextPos <= MAX_HEIGHT) {
+        user.posY = nextPos;
+
+        drawGame(user.posX, user.posY);
+    }
+}
+
+function moveRight() {
+    let nextPos = user.posX + PLAYER_SPEED;
+    const MAX_WIDTH = WIDTH - CELL_SIZE;
+
+    if (nextPos <= MAX_WIDTH) {
+        user.posX = nextPos;
+
+        drawGame(user.posX, user.posY);
+    }
+}
+
+function moveLeft() {
+    let nextPos = user.posX - PLAYER_SPEED;
+
+    if (nextPos >= CELL_SIZE) {
+        user.posX = nextPos;
+
+        drawGame(user.posX, user.posY);
+    }
+}
+
+function initGame() {
+    let startPos = 30;
+
+    user.posX = startPos;
+    user.posY = startPos;
+    drawGame(user.posX, user.posY);
+}
+
+function drawGame(currX, currY) {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    drawField();
+    drawPlayer(currX, currY);
+}
+
+function drawPlayer(currX, currY) {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(currX, currY, CELL_SIZE, CELL_SIZE);
+}
 
 function drawField() {
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
-    var spriteBlock = new Image(); // "Создаём" изображение
-    spriteBlock.src = 'img/sprites/sprite1.png'; // Источник изображения
-
-
-
-    for (var currPosY = 1; currPosY < COUNT_OF_CELLS_HEIGHT; ++currPosY) {
-        for (var currPosX = 1; currPosX < COUNT_OF_CELLS_WIDTH; ++currPosX) {
+    for (let currPosY = 1; currPosY < (COUNT_OF_CELLS_HEIGHT - 1); ++currPosY) {
+        for (let currPosX = 1; currPosX < COUNT_OF_CELLS_WIDTH; ++currPosX) {
             ctx.fillStyle = 'green';
             ctx.fillRect( (currPosX * CELL_SIZE), (currPosY * CELL_SIZE), CELL_SIZE, CELL_SIZE)
         }
     }
 
-    var yPos = 0;
+    spriteBlock.onload = function () { // Событие onLoad, ждём момента пока загрузится изображение
+        drawIronBlock();
 
-    for (var currPos = 0; currPos < COUNT_OF_CELLS_WIDTH; ++currPos) {
-        var xPos = currPos * CELL_SIZE;
+        // drawCementBlock()
+    }
+}
 
-        spriteBlock.onload = function () { // Событие onLoad, ждём момента пока загрузится изображение
-            ctx.drawImage(spriteBlock, 0, 0, CELL_SIZE, CELL_SIZE, xPos, yPos, CELL_SIZE, CELL_SIZE);// Рисуем изображение от точки с координатами 0, 0
-        };
+function drawIronBlock() {
+    for (let currPos = 0; currPos < COUNT_OF_CELLS_WIDTH; ++currPos) {
+        let xPos = currPos * CELL_SIZE;
 
+        ctx.drawImage(spriteBlock, 0, 0, CELL_SIZE, CELL_SIZE, xPos, 0, CELL_SIZE, CELL_SIZE); // Рисуем изображение от точки с координатами 0, 0
+    }
+
+    for (let currPos = 0; currPos < COUNT_OF_CELLS_WIDTH; ++currPos) {
+        let yPos = (COUNT_OF_CELLS_HEIGHT - 1) * CELL_SIZE;
+        let xPos = currPos * CELL_SIZE;
+
+        ctx.drawImage(spriteBlock, 0, 0, CELL_SIZE, CELL_SIZE, xPos, yPos, CELL_SIZE, CELL_SIZE);
+    }
+
+    for (let currPos = 1; currPos < COUNT_OF_CELLS_HEIGHT - 1; ++currPos) {
+        let yPos = currPos * CELL_SIZE;
+        let xPos = 0;
+
+        ctx.drawImage(spriteBlock, 0, 0, CELL_SIZE, CELL_SIZE, xPos, yPos, CELL_SIZE, CELL_SIZE);
+    }
+
+    let nextBlock = 2;
+
+    for (let currPosY = nextBlock; currPosY < (COUNT_OF_CELLS_HEIGHT - 1); currPosY = currPosY + nextBlock) {
+        let yPos = currPosY * CELL_SIZE;
+
+        for (let currPosX = nextBlock; currPosX < COUNT_OF_CELLS_WIDTH; currPosX = currPosX + nextBlock) {
+            let xPos = currPosX * CELL_SIZE;
+
+            ctx.drawImage(spriteBlock, 0, 0, CELL_SIZE, CELL_SIZE, xPos, yPos, CELL_SIZE, CELL_SIZE);
+        }
     }
 }
---
+++ Bomberman/Project/src/gameA.js
@@ -0,0 +1,40 @@
+var COUNT_OF_CELLS_WIDTH = 16;
+var COUNT_OF_CELLS_HEIGHT = 13;
+var WIDTH = 480;
+var HEIGHT = 390;
+var CELL_SIZE = 30;
+var score = 0;
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
+
+drawField();
+
+function drawField() {
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+    var spriteBlock = new Image(); // "Создаём" изображение
+    spriteBlock.src = 'img/sprites/sprite1.png'; // Источник изображения
+
+
+
+    for (var currPosY = 1; currPosY < COUNT_OF_CELLS_HEIGHT; ++currPosY) {
+        for (var currPosX = 1; currPosX < COUNT_OF_CELLS_WIDTH; ++currPosX) {
+            ctx.fillStyle = 'green';
+            ctx.fillRect( (currPosX * CELL_SIZE), (currPosY * CELL_SIZE), CELL_SIZE, CELL_SIZE)
+        }
+    }
+
+    var yPos = 0;
+
+    for (var currPos = 0; currPos < COUNT_OF_CELLS_WIDTH; ++currPos) {
+        var xPos = currPos * CELL_SIZE;
+
+        someFunc(spriteBlock, ctx, xPos);
+    }
+}
+
+function someFunc(spriteBlock, ctx, xPos) {
+    spriteBlock.onload = function () { // Событие onLoad, ждём момента пока загрузится изображение
+        ctx.drawImage(spriteBlock, 0, 0, CELL_SIZE, CELL_SIZE, xPos, 0, CELL_SIZE, CELL_SIZE);// Рисуем изображение от точки с координатами 0, 0
+    };
+}
