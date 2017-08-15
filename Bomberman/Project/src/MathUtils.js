6620cd2279626d2c14c899d32d1a7ad0ce751bcf
Ilya Protasov <pfltriot@gmail.com>
2017-08-15 10:10:10 +0300

    Delete MathUtils.js

--- Bomberman/Project/src/MathUtils.js
+++
@@ -1,8 +0,0 @@
-class MathUtils {
-	static intersectsRects(a, b) {
-		return (a.left < (b.left + b.width)) &&
-			(b.left < (a.left + a.width)) &&
-			(a.top < (b.top + b.height)) &&
-			(b.top < (a.top + a.height));
-	};
-}
