7c5713a08b6e830e8fd145182c382c0dac566e95
Ilya Protasov <pfltriot@gmail.com>
2017-08-15 10:10:25 +0300

    Delete Player.js

--- Bomberman/Project/src/Player.js
+++
@@ -1,9 +0,0 @@
-class Player extends Creature {
-    constructor(_startTimeAnimation) {
-    	super();
-		this.bombCount = START_BOMB_COUNT;
-		super.sprites = playerSprites;
-		super.startTimeAnimation = _startTimeAnimation;
-		this.mooving = false;
-    }
-}
